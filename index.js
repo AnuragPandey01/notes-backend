a0fc7a3079988a3adfb1969c6b1de2843bf247ae
AnuragPandey01 <anuragpandey87187@gmail.com>
2024-02-11 10:04:06 +0530

    test

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 import express from 'express';
-
+import 'dotenv/config'
 
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.json({
+        message: 'Hello World'
+    })
 });
 
 
-app.listen(port, () => {
+app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${port}`);
 })
